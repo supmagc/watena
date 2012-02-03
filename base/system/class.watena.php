e3cc122d85c447c01780abbd65894cfb0ceb171e
Jelle <supmagc@users.noreply.github.com>
2012-02-03 17:08:12 +0000

    Small changes

--- base/system/class.watena.php
+++ base/system/class.watena.php
@@ -114,7 +114,6 @@ class Watena extends Configurable {
 		set_include_path(get_include_path() . PATH_SEPARATOR . str_replace(',', PATH_SEPARATOR, self::getConfig('INCLUDE', '')));
 		Encoding::init(self::getConfig('CHARSET', 'UTF-8'));
 		Time::init(self::getConfig('TIMEZONE', 'UTC'));
-		ini_set('error_reporting', E_ALL);
 		if(!is_writable(PATH_DATA)) die('Data path is not writeable.');
 	}
 	
