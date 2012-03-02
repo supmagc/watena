6a8351928d16789191cac3a4918526c911c10bae
jelle <jelle@8a2756f3-d846-294c-82c5-f62e03536b7a>
2012-03-02 19:51:01 +0000

    UserManager addition

---
+++ libs/default/plugins/usermanager/class.user.php
@@ -0,0 +1,38 @@
+<?php
+
+class User {
+	
+	public function getId() {
+		
+	}
+	
+	public function getUserName() {
+		
+	}
+	
+	public function getUserEmail() {
+		
+	}
+	
+	public function isGuest() {
+		
+	}
+	
+	public function isRegistered() {
+		
+	}
+	
+	public function isActivated() {
+		
+	}
+	
+	public function isLoggedIn() {
+		
+	}
+	
+	public function isResettingPassword() {
+		
+	}
+}
+
+?>
