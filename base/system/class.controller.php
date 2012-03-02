436db20a47443781fd9179a974dfd768f888e737
Jelle <supmagc@users.noreply.github.com>
2012-03-02 23:10:07 +0000

    Some minor improvements to the mapping, and preparation to move some parts of Filter to Context

--- base/system/class.controller.php
+++ base/system/class.controller.php
@@ -4,12 +4,30 @@ abstract class Controller extends CacheableData {
 	
 	abstract public function process(Model $oModel, View $oView);
 	
-	public function redirect($sLocation) {
-		
+	private $m_oRemapping;
+	
+	public final function redirect($sLocation) {
+		header('location: ' . $sLocation, true);
 	}
 	
-	public function operationalMessage($sMessage) {
-		echo '<div style="text-align:center; border:1px solid #000; padding:5px; font-weight:bold;">'.$sMessage.'</div>';
+	public final function setRemapping($sMapping) {
+		$this->m_oRemapping = new Mapping($sMapping);
+	}
+	
+	public final function clearRemapping() {
+		$this->m_oRemapping = null;
+	}
+	
+	public final function getRemapping() {
+		return $this->m_oRemapping;
+	}
+	
+	public final function hasRemapping() {
+		return $this->m_oRemapping !== null;
+	}
+	
+	public final function operationalMessage($sMessage) {
+		echo '<div style="background: #CCC; text-align:center; border:1px solid #000; padding:5px; font-weight:bold;">'.$sMessage.'</div>';
 	}
 }
 
