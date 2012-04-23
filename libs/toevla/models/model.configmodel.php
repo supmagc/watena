55da8a51b8a6976f6920ec94af992f44b6053192
Jelle <supmagc@users.noreply.github.com>
2012-04-23 16:09:54 +0000

    Some stuff

--- libs/toevla/models/model.configmodel.php
+++ libs/toevla/models/model.configmodel.php
@@ -5,13 +5,14 @@ class ConfigModel extends Model {
 	
 	public function getConfigData() {
 		$aData = array(
-			'developmentLogin' => '' . new Mapping('/debug/login'),
-			'logoutUrl' => '' . new Mapping('/logout'),
-			'smartFoxSettings' => Encoding::indexOf($this->getWatena()->getMapping()->getHost(), '.com') ? 'ONLINE_TESTING' : ($_SERVER['COMPUTERNAME'] == 'JELLE-MONSTER' ? 'LOCALHOST' : 'GRINTERNAL'),
-			'imageUrlPrefix' => '' . new Mapping('/files/toevla/festival'),
-			'picasaUrlPrefix' => '' . new Mapping('/request/picasa'),
-			'flickrUrlPrefix' => '' . new Mapping('/request/flickr'),
-			'audioUrlPrefix' => '' . new Mapping('/files/toevla/audio')
+			'host'				=> '' . new Mapping(''),
+			'developmentLogin'	=> '' . new Mapping('/debug/login'),
+			'logoutUrl'			=> '' . new Mapping('/logout'),
+			'smartFoxSettings'	=> Encoding::indexOf($this->getWatena()->getMapping()->getHost(), '.com') ? 'ONLINE_TESTING' : ($_SERVER['COMPUTERNAME'] == 'JELLE-MONSTER' ? 'LOCALHOST' : 'GRINTERNAL'),
+			'imageUrlPrefix'	=> '' . new Mapping('/files/toevla/festival'),
+			'picasaUrlPrefix'	=> '' . new Mapping('/request/picasa'),
+			'flickrUrlPrefix'	=> '' . new Mapping('/request/flickr'),
+			'audioUrlPrefix'	=> '' . new Mapping('/files/toevla/audio')
 		);
 		$oStatement = DatabaseManager::getConnection('toevla')->select('game_config');
 		foreach($oStatement as $aRow) {
