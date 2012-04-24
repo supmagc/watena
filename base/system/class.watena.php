8fe560e7d834c212eb61f3652423da85f976b14e
jelle <jelle@8a2756f3-d846-294c-82c5-f62e03536b7a>
2012-04-24 21:15:05 +0000

    Path resolving optimized

--- base/system/class.watena.php
+++ base/system/class.watena.php
@@ -93,16 +93,16 @@ class Watena extends Configurable {
 	public final function getPath($sPath, $bVerify = true) {
 		$aMatches = array();
 		$aPositions = array();
-		if(Encoding::regFind('^([brdlBRDL]):([^/\\\\].*?)(/?)$', '' . $sPath, $aMatches, $aPositions)) {
+		if(Encoding::regFind('^[brdlBRDL][:/\\\\]([^/\\\\].*?)/?$', '' . $sPath, $aMatches, $aPositions)) {
 			switch($aMatches[1]) {
 				case 'b' :
-				case 'B' : $sPath = PATH_BASE . (Encoding::length($aMatches[2]) > 0 ? "/$aMatches[2]" : ''); break;
+				case 'B' : $sPath = PATH_BASE . (Encoding::length($aMatches[1]) > 0 ? "/$aMatches[1]" : ''); break;
 				case 'd' :
-				case 'D' : $sPath = PATH_DATA . (Encoding::length($aMatches[2]) > 0 ? "/$aMatches[2]" : ''); break;
+				case 'D' : $sPath = PATH_DATA . (Encoding::length($aMatches[1]) > 0 ? "/$aMatches[1]" : ''); break;
 				case 'r' :
-				case 'R' : $sPath = PATH_ROOT . (Encoding::length($aMatches[2]) > 0 ? "/$aMatches[2]" : ''); break;
+				case 'R' : $sPath = PATH_ROOT . (Encoding::length($aMatches[1]) > 0 ? "/$aMatches[1]" : ''); break;
 				case 'l' :
-				case 'L' : $sPath = PATH_LIBS . (Encoding::length($aMatches[2]) > 0 ? "/$aMatches[2]" : ''); break;
+				case 'L' : $sPath = PATH_LIBS . (Encoding::length($aMatches[1]) > 0 ? "/$aMatches[1]" : ''); break;
 			}
 		}
 		if($bVerify) {
