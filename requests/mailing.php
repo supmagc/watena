4284c328359b397287123f2eec9a24ac3116ae6c
Jelle <supmagc@users.noreply.github.com>
2012-05-11 07:42:14 +0000

    Some remaining stuff from the mailer

--- requests/mailing.php
+++ requests/mailing.php
@@ -1,5 +1,5 @@
 <?php
-define('MAILCOUNT', 0);
+define('MAILCOUNT', 1);
 define('NMVC', true);
 include '../base/watena.php';
 
@@ -9,13 +9,13 @@ $oTable = $oConnection->getTable('festival');
 $oStatement = $oTable->select();
 
 while(($oData = $oStatement->fetchObject()) !== false) {
-	if($oData->adminMail && $oData->adminName && $oData->mailCount <= MAILCOUNT) {
+	if($oData->adminMail && $oData->adminName && $oData->mailCount < MAILCOUNT) {
 		$oMail = new Mail();
 		$oMail->setTo($oData->adminMail, $oData->adminName);
 		$oMail->setFrom('info@flandersisafestival.com', 'Flanders Is A Festival');
 		$oMail->setSubject('Festival editor: Flanders is a Festival');
 		$sContent = file_get_contents(dirname(__FILE__) . '/mailing.html');
-		$sContent = Encoding::replace(array('{name}', '{linkExe}', '{linkPreview}'), array($oData->adminName, new Mapping('/festival/download/' . $oData->hash), new Mapping('/iframe/' . $oData->hash)), $sContent);
+		$sContent = Encoding::replace(array('{name}', '{linkExe}', '{linkPreview}'), array($oData->adminName, 'http://flandersisafestival.com/festival/download/' . $oData->hash, 'http://flandersisafestival.com/iframe/' . $oData->hash), $sContent);
 		$oMail->setContentHtml($sContent);
 		$oMail->convertHtmlToText();
 		//$oMail->setContentHtml(null);
@@ -27,8 +27,11 @@ while(($oData = $oStatement->fetchObject()) !== false) {
 		if(isset($_GET['action'])) {
 			if($_GET['action'] == 'test')
 				$oMail->setTo('supmagc@gmail.com', 'Jelle Voet');
-			if($_GET['action'] == 'send' || $_GET['action'] == 'test')
+			if($_GET['action'] == 'send' || $_GET['action'] == 'test') {
 				$oMail->send();
+				if($_GET['action'] == 'send')
+					$oTable->update(array('mailCount' => MAILCOUNT), $oData->ID);
+			}
 			if($_GET['action'] == 'test')
 				break;
 		}
