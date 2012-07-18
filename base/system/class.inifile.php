516340d2f610a81948d87d25f5227c7acd26423d
jelle <jelle@8a2756f3-d846-294c-82c5-f62e03536b7a>
2012-07-18 15:47:00 +0000

    Working IniFile class, and implemented (partially) ... what about the watena.ini ... ?

--- base/system/class.inifile.php
+++ base/system/class.inifile.php
@@ -3,24 +3,77 @@
 class IniFile extends CacheableFile {
 	
 	private $m_aData = array();
-	
-	public function make() {
-		// Read the data
-		$aData = parse_ini_file($this->getFilePath(), true);
+	private $m_aInheritance = array();
 		
-		// Make sure the data returned is a valid array
-		if($aData === false) {
-			$this->getLogger()->warning('Unableto parse *.ini file: {path}', array('path' => $this->getFilePath()));
+	public function make() {
+		$aParsing = parse_ini_file($this->getFilePath(), true);
+				
+		if($aParsing === false) {
+			$this->getLogger()->warning('Unable to parse *.ini file: {path}', array('path' => $this->getFilePath()));
 		}
 		else {
-			// Process the array
-			$aData = array_change_key_case($aData, CASE_LOWER);
-			$this->m_aData = $aData;
+			foreach($aParsing as $sSection => $aData) {
+				if(is_array($aData)) {
+					$this->processSection($sSection, $aData);
+				}
+				else {
+					$this->getLogger()->warning('Invalid/none-section data found in *.ini file: {path}', array('path' => $this->getFilePath()));
+				}
+			}
 		}
 	}
 	
-	public function getData() {
-		return $this->m_aData;
+	public function getData($sSection) {
+		if(isset($this->m_aData[$sSection])) {
+			$aData = $this->m_aData[$sSection];
+			$sInheritance = $this->m_aInheritance[$sSection];
+			if($sInheritance) return $this->merge($this->getData($sInheritance), $aData);
+			else return $aData;
+		}
+		else {
+			$this->getLogger()->debug('Unknown ini-section \'{section}\' in {file}', array('section' => $sSection, 'file' => $this->getFilePath()));
+			return array();
+		}
+	}
+	
+	private function processSection($sSection, array $aData) {
+		$sInheritance = null;
+		$sSection = Encoding::toLower($sSection);
+		$nExtendIndex = Encoding::indexOf($sSection, ':');
+		
+		if($nExtendIndex !== false) {
+			$sInheritance = Encoding::trim(Encoding::substring($sSection, $nExtendIndex + 1));
+			$sSection = Encoding::trim(Encoding::substring($sSection, 0, $nExtendIndex));
+		}
+		
+		if(!isset($this->m_aData[$sSection])) {
+			$this->m_aData[$sSection] = array();
+		}
+		
+		if(isset($this->m_aInheritance[$sSection]) && $this->m_aInheritance[$sSection] !== $sInheritance) {
+			$this->getLogger()->warning("Invalid ini-section inheritance, was '{section}' was previously defined as '{old}', and now '{new}'.", array(
+				'section' => $sSection,
+				'old' => $this->m_aInheritance[$sSection],
+				'new' => $sInheritance
+			));
+		}
+		
+		$this->m_aInheritance[$sSection] = $sInheritance;		
+		$this->m_aData[$sSection] = $this->merge($this->m_aData[$sSection], $this->processData($aData));
+	}
+	
+	private function processData(array $aData) {
+		$aReturn = array();
+		foreach($aData as $sKey => $mValue) {
+			array_assure($aReturn, explode_trim('.', Encoding::toLower($sKey)), $mValue, true);
+		}
+		return $aReturn;
+	}
+	
+	private function merge(array $a, array $b) {
+		foreach($b as $k => $v)
+			$a[$k] = (is_assoc($a[$k]) && is_assoc($v)) ? $this->merge($a[$k], $v) : $v;
+		return $a;
 	}
 }
 
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
+
+if($_SERVER['COMPUTERNAME'] == 'GRIN2011')
+	define('CONFIG', 'grin');
+
 include 'base/watena.php';
 ?>
