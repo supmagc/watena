d01b4736bd323133cc2e5e4e9e9846cbec33410d
jelle <jelle@8a2756f3-d846-294c-82c5-f62e03536b7a>
2012-07-26 21:56:04 +0000

    CacheLoader

--- base/system/class.cacheloader.php
+++ base/system/class.cacheloader.php
@@ -2,16 +2,29 @@
 
 class CacheLoader extends Object {
 	
-	public final function __construct($sObject, $sIdentifier = null) {
-		if(!class_exists($sObject)) $this->getLogger()->error($sMessage);
+	private $m_sClassName;
+	private $m_nLastChanged;
+	
+	public final function __construct($sClassName, $sIdentifier = null) {
+		if(!class_exists($sClassName, false)) {
+			$this->getLogger()->error('The class \'{class}\' you want to load CacheLoader does not exists.', array('class' => $sClassName));			
+		}
+		else if(in_array('Cacheable', class_parents($sClassName, false))) {
+			$this->getLogger()->error('The class \'{class}\' you want to load CacheLoader does not extend Cacheable.', array('class' => $sClassName));			
+		}
+		else {
+			$this->m_sClassName = $sClassName;
+		}
 	}
 	
-	public function addFileDependency($sFilePath) {
-		
-	}
-	
-	public function addDirectoryDependency($sFilePath) {
-		
+	public function addPathDependency($sPath) {
+		$sPath = parent::getWatena()->getPath($sPath);
+		if(!file_exists($sPath)) {
+			$this->getLogger()->warning('The path-dependency \'{path}\' for the CacheLoader does not exists.', array('path' => $sPath));
+		}
+		else {
+			$this->m_nLastChanged = max($this->m_nLastChanged, filemtime($sPath));
+		}
 	}
 	
 	public function get() {
