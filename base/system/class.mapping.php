d9317b8107e4691f048f7c9c07179e74e40f0b07
Jelle <supmagc@users.noreply.github.com>
2012-09-26 13:08:29 +0000

    latest changes

--- base/system/class.mapping.php
+++ base/system/class.mapping.php
@@ -8,40 +8,35 @@ class Mapping extends Object {
 	
 	private static $s_aDefaults;
 	
+	/**
+	 * Create a new mapping instance.
+	 * You can specify the following types of local-mapping:
+	 * absolute: (starting with a /) A mapping relative to the root.
+	 * relative: (not starting with a /) A mapping relative to the directory of the current mapping.
+	 * current: (current mapping) Specify null (default-value).
+	 * 
+	 * @param string $sLocal The local mapping.
+	 * @param array $aParams Additional GET-parameters.
+	 */
 	public final function __construct($sLocal = null, array $aParams = array()) {
 		if(!is_array(self::$s_aDefaults)) {
-			self::$s_aDefaults = array();
-			self::$s_aDefaults['host'] = self::host();
-			self::$s_aDefaults['port'] = self::port();
-			self::$s_aDefaults['method'] = self::method();
-			self::$s_aDefaults['useragent'] = self::userAgent();
-			self::$s_aDefaults['offset'] = self::offset();
-				
-			if(isset($_SERVER['REDIRECT_URL'])) {
-				self::$s_aDefaults['local'] = Encoding::substring($_SERVER['REDIRECT_URL'], Encoding::length(self::$s_aDefaults['offset']));
-			}
-			else if(isset($_SERVER['REQUEST_URI'])) {
-				$nLength = Encoding::contains($_SERVER['REQUEST_URI'], '?') ? Encoding::indexOf($_SERVER['REQUEST_URI'], '?') - Encoding::length($this->m_aVariables['offset']) : null;
-				self::$s_aDefaults['local'] = urldecode(Encoding::substring($_SERVER['REQUEST_URI'], Encoding::length(self::$s_aDefaults['offset']), $nLength));
-			}
-			else {
-				self::$s_aDefaults['local'] = '/';
-			}
-				
-			self::$s_aDefaults['root'] = 'http://' . self::$s_aDefaults['host'] . (self::$s_aDefaults['port'] != 80 ? ':'.self::$s_aDefaults['port'] : '') . self::$s_aDefaults['offset'];
-			self::$s_aDefaults['parts'] = explode_trim('/', self::$s_aDefaults['local']);		
+			$this->getLogger()->error('Mapping not yet inited!');
 		}
-		
+
+		// Load defaults and params
 		$this->m_aVariables = self::$s_aDefaults;
+		$this->m_aParams = $aParams;
 		
+		// Parse localpart
 		if($sLocal) {
 			if(($nIndex = Encoding::indexOf($sLocal, '#'))) {
-				$this->m_sAnchor = Encoding::substring($sLocal, $nIndex + 1);
+				$this->m_aVariables['anchor'] = Encoding::substring($sLocal, $nIndex + 1);
 				$sLocal = Encoding::substring($sLocal, 0, $nIndex);
 			}			
 			
 			if(($nIndex = Encoding::indexOf($sLocal, '?'))) {
-				parse_str(Encoding::substring($sLocal, $nIndex + 1), $this->m_aParams);
+				parse_str(Encoding::substring($sLocal, $nIndex + 1), $aParams);
+				$this->m_aParams = array_merge($this->m_aParams, $aParams);
 				$sLocal = Encoding::substring($sLocal, 0, $nIndex);
 			}
 			
@@ -57,15 +52,15 @@ class Mapping extends Object {
 				$this->m_aVariables['parts'] = explode_trim('/', $this->m_aVariables['local']);
 			}
 		}
-		
-		$this->m_aVariables['full'] = $this->m_aVariables['root'] . $this->m_aVariables['local'];
-		
+
+		// Combine query and full url
+		$this->m_aVariables['full'] = $this->m_aVariables['root'] . $this->m_aVariables['local'];		
 		if(count($this->m_aParams) > 0) {
-			$this->m_aVariables['full'] .= '?' . http_build_query($this->m_aParams, null, '&');
+			$this->m_aVariables['query'] = http_build_query($this->m_aParams, null, '&');
+			$this->m_aVariables['full'] .= '?' . $this->m_aVariables['query'];
 		}
-		
-		if($this->m_sAnchor !== null) {
-			$this->m_aVariables['full'] .= '#' . $this->m_sAnchor;
+		if($this->m_aVariables['anchor'] !== null) {
+			$this->m_aVariables['full'] .= '#' . $this->m_aVariables['anchor'];
 		}
 	}
 	
@@ -77,11 +72,11 @@ class Mapping extends Object {
 	 */
 	public final function getVariable($sName) {
 		$sName = Encoding::toLower($sName);
-		return isset($this->m_aVariables[$sName]) ? $this->m_aVariables[$sName] : false;
+		return isset($this->m_aVariables[$sName]) ? $this->m_aVariables[$sName] : null;
 	}
 	
 	/**
-	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
 	 * => subdomain.example.org
 	 * 
 	 * @return string
@@ -91,7 +86,7 @@ class Mapping extends Object {
 	}
 	
 	/**
-	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
 	 * => https://subdomain.example.org[:80]
 	 * 
 	 * @return string
@@ -101,7 +96,7 @@ class Mapping extends Object {
 	}
 	
 	/**
-	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
 	 * => 80
 	 * 
 	 * @return string
@@ -111,7 +106,7 @@ class Mapping extends Object {
 	}
 	
 	/**
-	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
 	 * => /path-to-site
 	 * 
 	 * @return string
@@ -121,7 +116,7 @@ class Mapping extends Object {
 	}
 	
 	/**
-	* example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value
+	* example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
 	* => https://subdomain.example.org[:80]/path-to-site
 	*
 	* @return string
@@ -131,7 +126,7 @@ class Mapping extends Object {
 	}
 	
 	/**
-	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
 	 * => /directory/script.php
 	 * 
 	 * @return string
@@ -141,11 +136,12 @@ class Mapping extends Object {
 	}
 	
 	/**
-	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
 	 * => 0: directory
 	 * => 1: script.php
+	 * => 2: null
 	 * 
-	 * @return string
+	 * @return string|null
 	 */
 	public final function getPart($nIndex) {
 		$aParts = $this->getVariable('parts');
@@ -153,8 +149,39 @@ class Mapping extends Object {
 	}
 	
 	/**
-	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value
-	 * => https://subdomain.example.org[:80]/path-to-site/directory/script.php
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
+	 * => name=value
+	 *
+	 * @return string|null
+	 */
+	public final function getQuery() {
+		return $this->getVariable('query');
+	}
+	
+	/**
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
+	 * => name: value
+	 * => unknown: null
+	 *
+	 * @return string|null
+	 */
+	public final function getParam($sName) {
+		return isset($this->m_aParams[$sName]) ? $this->m_aParams[$sName] : null;
+	}
+	
+	/**
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
+	 * => anchor
+	 *
+	 * @return string|null
+	 */
+	public final function getAnchor() {
+		return $this->getVariable('anchor');
+	}
+	
+	/**
+	 * example: https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
+	 * => https://subdomain.example.org[:80]/path-to-site/directory/script.php?name=value#anchor
 	 * 
 	 * @return string
 	 */
@@ -162,28 +189,30 @@ class Mapping extends Object {
 		return $this->getVariable('full');
 	}
 	
-	public final function getAnchor() {
-		return $this->getVariable('anchor');
-	}
-	
-	public final function getQuery() {
-		return $this->getVariable('query');
-	}
-	
-	public final function getParam($sName) {
-		return isset($this->m_aParams) ? $this->m_aParams[$sName] : null;
-	}
-	
 	public final function getUseragent() {
 		return $this->getVariable('useragent');
 	}
-	
+
+	/**
+	 * Returns the full url of this instance
+	 * 
+	 * @see Mapping::getFull()
+	 * @return string
+	 */
 	public final function toString() {
 		return $this->getFull();
 	}
-	
+
+	/**
+	 * Retrieve the hostname of the current request.
+	 * Optionally you can try to retrieve the actual server-name as specified on the server.
+	 * If not requested, the value returned will the host-portion of the request is specified.
+	 * If none of these options are viable the return value will be 'unknown'.
+	 * 
+	 * @return string
+	 */
 	public final static function host($bInternal = false) {
-		if($bInternal && !empty($_SERVER['SERVER_NAME'])) {
+		if(($bInternal || empty($_SERVER['HTTP_HOST'])) && !empty($_SERVER['SERVER_NAME'])) {
 			return $_SERVER['SERVER_NAME'];
 		}
 		else if(!empty($_SERVER['HTTP_HOST'])) {
@@ -193,15 +222,35 @@ class Mapping extends Object {
 			return 'unknown';
 		}
 	}
-	
+
+	/**
+	 * Retrieve the portnumber of the current request.
+	 * If no port is specified, this will default to 80.
+	 * 
+	 * @return int
+	 */
 	public final static function port() {
 		return !empty($_SERVER['SERVER_PORT']) ? $_SERVER['SERVER_PORT'] : 80;
 	}
-	
+
+	/**
+	 * Retrieve the request-method of the current request.
+	 * If no request-method is specified, this will default to GET.
+	 * 
+	 * @return string
+	 */
 	public final static function method() {
 		return !empty($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET'; 
 	}
-	
+
+	/**
+	 * Retrieve the useragent of the current request.
+	 * This will automatically save the useragent to a session.
+	 * If a subsequent request with useragent * / * should occur, the session-value will be used instead.
+	 * If no useragent is specified, this will default to 'Unknown'.
+	 * 
+	 * @return string
+	 */ 
 	public final static function userAgent() {
 		if(!empty($_SERVER['HTTP_USER_AGENT'])) {
 			if($_SERVER['HTTP_USER_AGENT'] !== '*/*') {
@@ -217,8 +266,44 @@ class Mapping extends Object {
 		}
 	}
 	
+	/**
+	 * Retrieve the offset of the current request.
+	 * The offset is the mapping from the host to the root (index.php and .htaccess) of the framework.
+	 * 
+	 * @return string
+	 */
 	public final static function offset() {
 		return !empty($_SERVER['DOCUMENT_ROOT']) ? Encoding::replace('\\', '/', Encoding::substring(PATH_ROOT, Encoding::length($_SERVER['DOCUMENT_ROOT']))) : '';
+	}
+
+	/**
+	 * Initialize the mapping by calculating the current request values.
+	 * This must be called before any instance creation of this class.
+	 */
+	public final static function init() {
+		self::$s_aDefaults = array();
+		self::$s_aDefaults['query'] = null;
+		self::$s_aDefaults['anchor'] = null;
+		self::$s_aDefaults['host'] = self::host();
+		self::$s_aDefaults['port'] = self::port();
+		self::$s_aDefaults['method'] = self::method();
+		self::$s_aDefaults['useragent'] = self::userAgent();
+		self::$s_aDefaults['offset'] = self::offset();
+		
+		if(isset($_SERVER['REDIRECT_URL'])) {
+			self::$s_aDefaults['local'] = Encoding::substring($_SERVER['REDIRECT_URL'], Encoding::length(self::$s_aDefaults['offset']));
+		}
+		else if(isset($_SERVER['REQUEST_URI'])) {
+			$nLengthOffset = Encoding::length($this->m_aVariables['offset']);
+			$nLengthLocal = Encoding::contains($_SERVER['REQUEST_URI'], '?') ? (Encoding::indexOf($_SERVER['REQUEST_URI'], '?') - $nLengthOffset) : null;
+			self::$s_aDefaults['local'] = urldecode(Encoding::substring($_SERVER['REQUEST_URI'], $nLengthOffset, $nLengthLocal));
+		}
+		else {
+			self::$s_aDefaults['local'] = '/';
+		}
+		
+		self::$s_aDefaults['root'] = 'http://' . self::$s_aDefaults['host'] . (self::$s_aDefaults['port'] != 80 ? ':'.self::$s_aDefaults['port'] : '') . self::$s_aDefaults['offset'];
+		self::$s_aDefaults['parts'] = explode_trim('/', self::$s_aDefaults['local']);
 	}
 }
 
--- testing/MappingTest.php
+++ testing/MappingTest.php
@@ -4,38 +4,43 @@ class MappingTest extends PHPUnit_Framework_TestCase {
 	
 	public function testDefault() {
 		$oMapping = watena()->getMapping();
-		$this->assertEquals($oMapping->getRoot(), 'http://unknown');
-		$this->assertEquals($oMapping->getFull(), 'http://unknown/');
-		$this->assertEquals($oMapping->getHost(), 'unknown');
-		$this->assertEquals($oMapping->getLocal(), '/');
-		$this->assertEquals($oMapping->getOffset(), '');
-		$this->assertEquals($oMapping->getPort(), 80);
-		$this->assertEquals($oMapping->getUseragent(), 'Unknown');
+		$this->assertEquals('http://unknown', $oMapping->getRoot());
+		$this->assertEquals('http://unknown/', $oMapping->getFull());
+		$this->assertEquals('unknown', $oMapping->getHost());
+		$this->assertEquals('/', $oMapping->getLocal());
+		$this->assertEquals('', $oMapping->getOffset());
+		$this->assertEquals(80, $oMapping->getPort());
+		$this->assertEquals('Unknown', $oMapping->getUseragent());
 	}
 	
 	public function testAbsoluteSubDirectory() {
 		$oMapping = new Mapping('/test/a/bla.mine');
-		$this->assertEquals($oMapping->getLocal(), '/test/a/bla.mine');
-		$this->assertEquals($oMapping->getPart(0), 'test');
-		$this->assertEquals($oMapping->getPart(1), 'a');
-		$this->assertEquals($oMapping->getPart(2), 'bla.mine');
+		$this->assertEquals('/test/a/bla.mine', $oMapping->getLocal());
+		$this->assertEquals('test', $oMapping->getPart(0));
+		$this->assertEquals('a', $oMapping->getPart(1));
+		$this->assertEquals('bla.mine', $oMapping->getPart(2));
+		$this->assertNull($oMapping->getAnchor());
 		$this->assertNull($oMapping->getPart(3));
 	}
 	
 	public function testRelativeSubDirectory() {
 		$oMapping = new Mapping('test/a/bla.mine');
-		$this->assertEquals($oMapping->getLocal(), '/test/a/bla.mine');
-		$this->assertEquals($oMapping->getPart(0), 'test');
-		$this->assertEquals($oMapping->getPart(1), 'a');
-		$this->assertEquals($oMapping->getPart(2), 'bla.mine');
+		$this->assertEquals('/test/a/bla.mine', $oMapping->getLocal());
+		$this->assertEquals('test', $oMapping->getPart(0));
+		$this->assertEquals('a', $oMapping->getPart(1));
+		$this->assertEquals('bla.mine', $oMapping->getPart(2));
+		$this->assertNull($oMapping->getAnchor());
 		$this->assertNull($oMapping->getPart(3));
 	}
 	
 	public function testParams() {
 		$oMapping = new Mapping('helloworld?oeps=noope#linkage');
-		$this->assertEquals($oMapping->getLocal(), '/helloworld');
-		$this->assertEquals($oMapping->getPart(0), 'helloworld');
-		$this->assertEquals($oMapping->getFull(), 'http://unknown/helloworld?oeps=noope#linkage');
+		$this->assertEquals('/helloworld', $oMapping->getLocal());
+		$this->assertEquals('helloworld', $oMapping->getPart(0));
+		$this->assertEquals('noope', $oMapping->getParam('oeps'));
+		$this->assertEquals('linkage', $oMapping->getAnchor());
+		$this->assertEquals('http://unknown/helloworld?oeps=noope#linkage', $oMapping->getFull());
+		$this->assertNull($oMapping->getParam('unknown'));
 		$this->assertNull($oMapping->getPart(1));
 	}
 }
