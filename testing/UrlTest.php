398dc8c349901224f987b6574679c5d9aade6e45
Jelle <supmagc@users.noreply.github.com>
2012-11-18 22:46:58 +0000

    New Url class

--- testing/UrlTest.php
+++ testing/UrlTest.php
@@ -2,11 +2,13 @@
 
 class UrlTest extends PHPUnit_Framework_TestCase {
 	
+	const URL = 'proto://user:pass@example.com:123/path/dir/file.xs?myStr=test&myVal=123#placeHolder';
+	
 	public function setUp() {
 	}
 	
 	public function testDefault() {
-		$oUrl = new Url('proto://user:pass@example.com:123/path/dir/file.xs?myStr=test&myVal=123#placeHolder');
+		$oUrl = new Url(self::URL);
 		
 		$this->assertEquals('proto', $oUrl->getScheme());
 		$this->assertEquals('user', $oUrl->getUserName());
@@ -17,6 +19,34 @@ class UrlTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('test', $oUrl->getParameter('myStr'));
 		$this->assertEquals(123, $oUrl->getParameter('myVal'));
 		$this->assertEquals('placeHolder', $oUrl->getAnchor());
+		$this->assertEquals(self::URL, $oUrl->toString());
+	}
+	
+	public function testSetAndGet() {
+		$oUrl = new Url(self::URL);
+		
+		$oUrl->setScheme('ftp');
+		$this->assertEquals('ftp', $oUrl->getScheme());
+		
+		$oUrl->setUserName('Jelle Voet');
+		$this->assertEquals('Jelle Voet', $oUrl->getUserName());
+		
+		$oUrl->setUserName('Jelle Voet');
+		$this->assertEquals('Jelle Voet', $oUrl->getUserName());
+		
+		$oUrl->setUserName('Jelle Voet');
+		$this->assertEquals('Jelle Voet', $oUrl->getUserName());
+		
+		$oUrl->setUserName('Jelle Voet');
+		$this->assertEquals('Jelle Voet', $oUrl->getUserName());
+		
+		$oUrl->setUserName('Jelle Voet');
+		$this->assertEquals('Jelle Voet', $oUrl->getUserName());
+		
+		$oUrl->setUserName('Jelle Voet');
+		$this->assertEquals('Jelle Voet', $oUrl->getUserName());
+		
+		$this->assertEquals('ftp://Jelle', $oUrl->toString());
 	}
 	
 	public function tearDown() {
