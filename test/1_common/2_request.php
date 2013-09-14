9cf57e0643fd4a3089a4d0dce4e91738946d33c7
Jelle <supmagc@users.noreply.github.com>
2013-09-14 23:15:37 +0000

    test

--- test/1_common/2_request.php
+++ test/1_common/2_request.php
@@ -3,43 +3,65 @@ include_once realpath(dirname(__FILE__) . '/../../base/system/static.request.php
 
 class RequestTest extends Test {
 	
+	private $m_sProtocol;
+	private $m_nProtocolPort;
+	private $m_sHost;
+	private $m_nPort;
+	private $m_sOffset;
+	
 	public function setup() {
+		$this->m_sProtocol = 'http';
+		$this->m_nProtocolPort = 80;
+		$this->m_sHost = Encoding::toLower($_SERVER['HTTP_HOST']);
+		$this->m_nPort = (int)$_SERVER['SERVER_PORT'];
+		$this->m_sOffset = '/watena'; // TODO: make this variable
+		if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+			$this->m_sProtocol = 'https';
+			$this->m_nProtocolPort = 443;
+		}
+		
 		Request::init();
 	}
 
 	public function testIsHttp() {
-		$this->assertTrue(Request::isHttp());
+		if('http' == $this->m_sProtocol && $this->assertTrue(Request::isHttp())) {
+			$this->assertEquals('http', Request::protocol());
+		}
 	}
 	
 	public function testIsHttps() {
-		$this->assertFalse(Request::isHttps());
+		if('https' == $this->m_sProtocol && $this->assertTrue(Request::isHttps())) {
+			$this->assertEquals('https', Request::protocol());
+		}
 	}
 	
 	public function testProtocol() {
-		if(Request::isHttp())
-			$this->assertEquals('http', Request::protocol());
-		if(Request::isHttps())
-			$this->assertEquals('https', Request::protocol());
+		$this->assertEquals($this->m_sProtocol, Request::protocol());
 	}
 	
 	public function testHost() {
-		$this->assertEquals(Encoding::toLower($_SERVER['HTTP_HOST']), Request::host());
+		$this->assertEquals($this->m_sHost, Request::host());
 	}
 	
 	public function testPort() {
-		$this->assertEquals(80, Request::port());
+		$this->assertEquals($this->m_nPort, Request::port());
 	}
 	
 	public function testBase() {
-		$this->assertEquals('http://localhost', Request::base());
+		$sHost = $this->m_sProtocol . '://' . $this->m_sHost;
+		if($this->m_nPort != $this->m_nProtocolPort) $sHost .= ':' . $this->m_nPort;
+		$this->assertEquals($sHost, Request::base());
 	}
 	
 	public function testOffset() {
-		$this->assertEquals('', Request::offset());
+		$this->assertEquals($this->m_sOffset, Request::offset());
 	}
 	
 	public function testRoot() {
-		$this->assertEquals('http://localhost', Request::root());
+		$sRoot = $this->m_sProtocol . '://' . $this->m_sHost;
+		if($this->m_nPort != $this->m_nProtocolPort) $sRoot .= ':' . $this->m_nPort;
+		$sRoot .= $this->m_sOffset;
+		$this->assertEquals($sRoot, Request::root());
 	}
 	
 	public function testPath() {
@@ -47,7 +69,10 @@ class RequestTest extends Test {
 	}
 	
 	public function testUrl() {
-		$this->assertEquals('http://localhost/w_test.php', Request::url());
+		$sUrl = $this->m_sProtocol . '://' . $this->m_sHost;
+		if($this->m_nPort != $this->m_nProtocolPort) $sUrl .= ':' . $this->m_nPort;
+		$sUrl .= $this->m_sOffset . '/w_test.php';
+		$this->assertEquals($sUrl, Request::url());
 	}
 	
 	public function testMethod() {
