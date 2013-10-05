85276731d4b1756bf8a1b6e26b07cb387a89580c
Jelle <supmagc@users.noreply.github.com>
2013-10-05 23:13:58 +0000

    Integrating ajax response with mvc

--- libs/default/views/view.ajaxview.php
+++ libs/default/views/view.ajaxview.php
@@ -3,10 +3,23 @@
 class AjaxView extends View {
 	
 	public function headers(Model $oModel = null) {
-		
+		$oModel->headers();
 	}
 	
 	public function render(Model $oModel = null) {
+		
+		if(!$oModel)
+		$this->getLogger()->error('No model/data was given.');
+		
+		if(!$this->m_sMethod)
+			$this->getLogger()->error('No method-parameter was given in the view-config.');
+					
+		if(!method_exists($oModel, $this->m_sMethod))
+			$this->getLogger()->error('Unable to find the required method "{method}" in the given Model-object.', array('method' => $this->m_sMethod));
+		
+		$sContent = json_encode(call_user_func(array($oModel, $this->m_sMethod)));
+		echo $sContent;
+		
 		$aData = array(
 			'errors' => $oModel->getErrors()
 		);
