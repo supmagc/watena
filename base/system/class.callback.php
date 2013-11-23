4c8fad6af5dee6daebbf94001c650cf71b67b43b
jelle <jelle@8a2756f3-d846-294c-82c5-f62e03536b7a>
2013-11-23 17:27:27 +0000

    initial callback testing

---
+++ base/system/class.callback.php
@@ -0,0 +1,44 @@
+<?php
+
+class Callback extends Object {
+	
+	private $m_sMethod;
+	private $m_aArguments;
+	
+	public function __construct($sMethod, array $aArguments = array()) {
+		$this->m_sMethod = $sMethod;
+		$this->m_aArguments = $aArguments;
+	}
+	
+	public final function getMethod() {
+		return $this->m_sMethod;
+	}
+	
+	public final function getArguments() {
+		return $this->m_aArguments;
+	}
+	
+	public final function getArgument($nIndex, $mDefault = null) {
+		return ($nIndex >= 0 && $nIndex < count($this->m_aArguments)) ? $this->m_aArguments[$nIndex] : $mDefault;
+	}
+	
+	public final function getArgumentsLength() {
+		return count($this->m_aArguments);
+	}
+	
+	public static function LoadFromRequest() {
+		$aData = array();
+		if(isset($_GET['method'])) $aData = $_GET;
+		if(isset($_POST['method'])) $aData = $_POST;
+		
+		if(!empty($aData)) {
+			$sMethod = $aData['method'];
+			return new Callback($sMethod);
+		}
+		else {
+			return null;
+		}
+	}
+}
+
+?>
