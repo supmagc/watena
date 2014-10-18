86f17a4b8ba0a57ff99613d518b6e8e2d64372c8
jelle <jelle@8a2756f3-d846-294c-82c5-f62e03536b7a>
2014-10-18 22:28:05 +0000

    documentation update

--- docs/html/search/classes_68.js
+++ docs/html/search/classes_68.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['html2text',['html2text',['../classhtml2text.html',1,'']]]
+  ['html2text',['html2text',['../classhtml2text.html',1,'']]],
+  ['htmlmodel',['HtmlModel',['../class_html_model.html',1,'']]]
 ];
--- docs/html/search/functions_63.js
+++ docs/html/search/functions_63.js
@@ -3,6 +3,7 @@ var searchData=
   ['checksignature',['checkSignature',['../class_o_auth_signature_method.html#aedd9a3e183880b2d1ea47fe06c988240',1,'OAuthSignatureMethod']]],
   ['clearallpersistentdata',['clearAllPersistentData',['../class_base_facebook.html#a2f98dbe9b9b63d3e76ba7f1971d80c91',1,'BaseFacebook']]],
   ['clearpersistentdata',['clearPersistentData',['../class_base_facebook.html#a3bfb6ef7b1abbd3ec0545be109d32bef',1,'BaseFacebook']]],
+  ['coarsecacheidentifier',['coarseCacheIdentifier',['../class_cacheable.html#ab6090f0ed75d626d969d6f88b0cbf882',1,'Cacheable']]],
   ['cookie',['cookie',['../class_request.html#ad6b7fe8e73dd7eed912086488002ddd0',1,'Request']]],
   ['createtemplateclass',['createTemplateClass',['../class_template_file.html#ad6ded534b21256f5c40f02d19504bcaf',1,'TemplateFile']]],
   ['createtemplatefile',['createTemplateFile',['../class_template_file.html#ad40fa812d27ae5ee64c2ae49dc98f10d',1,'TemplateFile']]]
