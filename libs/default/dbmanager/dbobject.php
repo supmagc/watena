de8dc676b9e6a39750ad6e56f2e4eae3b31013d3
jelle <jelle@8a2756f3-d846-294c-82c5-f62e03536b7a>
2015-01-05 22:52:13 +0000

    DB stuff

--- libs/default/dbmanager/dbobject.php
+++ libs/default/dbmanager/dbobject.php
@@ -8,6 +8,12 @@ class DbObject extends Object {
 	
 	private static $s_aObjectInstances = array();
 	
+	/**
+	 * Internal constructor for an object representing a table row, identifiablme by a single column.
+	 * 
+	 * @param DbTable $oTable Table object contraining table name and default ID column.
+	 * @param array $aData An array with the row data.
+	 */
 	protected final function __construct(DbTable $oTable, array $aData) {
 		parent::__construct();
 		$this->m_oTable = $oTable;
@@ -15,30 +21,71 @@ class DbObject extends Object {
 		self::$s_aObjectInstances[get_class($this)][$this->getId()] = $this;
 	}
 	
+	/**
+	 * Get the value for a specific column, or the default value if none is set.
+	 * 
+	 * @param string $sColumn Column name.
+	 * @param mixed $mDefault Default value when no column is found. (not null, since null is a valid sql-value)
+	 * @return mixed|false Returns $mDefault is when column is not found.
+	 */
 	protected function getDataValue($sColumn, $mDefault = false) {
-		return isset($this->m_aData[$sColumn]) ? $this->m_aData[$sColumn] : $mDefault;
+		return (!$this->m_aData && isset($this->m_aData[$sColumn])) ? $this->m_aData[$sColumn] : $mDefault;
 	}
-	
+
+	/**
+	 * Try to update the given value in this instance, and
+	 * reflect that change on the database-table.
+	 * 
+	 * @see DbTable::update()
+	 * @param string $sColumn
+	 * @param mixed $mValue
+	 * @return boolean
+	 */
 	protected function setDataValue($sColumn, $mValue) {
-		if($this->getTable()->update(array($sColumn => $mValue), $this->getId())) {
+		if(!$this->m_bDeleted && $this->getTable()->update(array($sColumn => $mValue), $this->getId())) {
 			$this->m_aData[$sColumn] = $mValue;
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 	
+	/**
+	 * Get the DbTable instance.
+	 * 
+	 * @return DbTable
+	 */
 	public function getTable() {
 		return $this->m_oTable;
 	}
 	
+	/**
+	 * Get the row ID.
+	 * 
+	 * @return mixed
+	 */
 	public function getId() {
 		return $this->m_aData[$this->getTable()->getIdField()];
 	}
 	
+	/**
+	 * Delete this row from the database, and flag this instance as deleted.
+	 */
 	public function delete() {
+		if($this->m_bDeleted) 
+			return;
+		
 		unset(self::$s_aObjectInstances[get_class($this)][$this->getId()]);
 		$this->getTable()->delete($this->getId());
 		$this->m_bDeleted = true;
 	}
 	
+	/**
+	 * Check if this instance is supposed to be deleted.
+	 * 
+	 * @return boolean
+	 */
 	public function isDeleted() {
 		return (bool)$this->m_bDeleted;
 	}
--- test/4_plugins/1_databasemanager.php
+++ test/4_plugins/1_databasemanager.php
@@ -1,16 +1,27 @@
 <?php
 require_plugin('DatabaseManager');
 
+class DbObjectTest extends DbObject {
+	
+}
+
 class DatabaseManagerTest extends Test {
 	
 	private $m_oConnection;
+	private $m_oTableSingle;
+	private $m_sRandomName;
 	
 	public function setup() {
 		$this->m_oConnection = DatabaseManager::getConnection('testing');
+		$this->m_oTableSingle = new DbTable($this->m_oConnection, 'table_single', 'ID');
+		$this->m_sRandomName = md5(time());
 	}
 	
 	public function testConnection() {
 		$this->assertType('DbConnection', $this->m_oConnection);
+		$this->assertEquals('testing', $this->m_oConnection->getUser());
+		$this->assertEquals('testing', $this->m_oConnection->getPass());
+		$this->assertEquals('testing', Encoding::toLower($this->m_oConnection->getIdentifier()));
 	}
 
 	public function testConnectionSerializable() {
@@ -18,8 +29,62 @@ class DatabaseManagerTest extends Test {
 		$this->m_oConnection = unserialize($sSerialized);
 		$this->assertType('DbConnection', $this->m_oConnection);
 	}
+
+	public function testConnectionConnect() {
+		$this->assertTrue($this->m_oConnection->isConnected());
+		$this->m_oConnection->disconnect();
+		$this->assertFalse($this->m_oConnection->isConnected());
+		$this->m_oConnection->connect();
+		$this->assertTrue($this->m_oConnection->isConnected());
+	}
+	
+	public function testTableSingle() {
+		$this->assertType('DbTable', $this->m_oTableSingle);
+		$this->assertEquals($this->m_oConnection->getIdentifier(), $this->m_oTableSingle->getConnection()->getIdentifier());
+		$this->assertEquals('table_single', $this->m_oTableSingle->getTable());
+		$this->assertEquals('ID', $this->m_oTableSingle->getIdField());
+	}
+	
+	public function testTableSingleSerializable() {
+		$sSerialized = serialize($this->m_oTableSingle);
+		$this->m_oTableSingle = unserialize($sSerialized);
+		$this->assertType('DbTable', $this->m_oTableSingle);
+		$this->assertEquals($this->m_oConnection->getIdentifier(), $this->m_oTableSingle->getConnection()->getIdentifier());
+	}
+	
+	public function testTableSingleOperations() {
+		$nInsert = $this->m_oTableSingle->insert(array(
+			'name_notnull' => 'r'.$this->m_sRandomName,
+			'name_null' => null));
+		$sInsert = $this->m_oTableSingle->insert(array(
+			'name_notnull' => 'n'.$this->m_sRandomName,
+			'name_null' => null), "name_notnull");
+		
+		$this->assertNumeric($nInsert);
+		$this->assertEquals('n'.$this->m_sRandomName, $sInsert);
+		
+		$this->assertTrue($this->m_oTableSingle->update(array(
+			'name_notnull' => 'rr'.$this->m_sRandomName
+		), $nInsert));
+		$this->assertTrue($this->m_oTableSingle->update(array(
+			'name_null' => 'nn'.$this->m_sRandomName
+		), $sInsert, 'name_notnull'));
+
+		$aRow1 = $this->m_oTableSingle->select($nInsert)->fetch(PDO::FETCH_ASSOC);
+		$aRow2 = $this->m_oTableSingle->select($sInsert, 'name_notnull')->fetch(PDO::FETCH_ASSOC);
+		
+		$this->assertEquals($nInsert, $aRow1['ID']);
+		$this->assertEquals($sInsert, $aRow2['name_notnull']);
+		$this->assertEquals('rr'.$this->m_sRandomName, $aRow1['name_notnull']);
+		$this->assertEquals('nn'.$this->m_sRandomName, $aRow2['name_null']);
+		$this->assertNull($aRow1['name_null']);
+		
+		$this->assertTrue($this->m_oTableSingle->delete($nInsert));
+		$this->assertTrue($this->m_oTableSingle->delete($sInsert, 'name_notnull'));
+	}
 	
 	public function teardown() {
 		$this->m_oConnection = null;
+		$this->m_oTableSingle = null;
 	}
 }
